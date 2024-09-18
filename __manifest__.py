6496c9ef95b7d181933825c903b93aabe6ba3718
rvalencia <rvalencia>
2024-09-18 11:43:21 -0600

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Dont limit variant",
+    "version": "15.0.1.0.0",
+    "author": "Munin",
+    "summary": "Sin limite de variantes",
+    "depends": ["product"],
+    "category": "stock",
+    "data": [
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
